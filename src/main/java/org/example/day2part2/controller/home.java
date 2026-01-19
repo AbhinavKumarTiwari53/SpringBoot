03c71f3c5618a8c402d69b26dbff12dcda462227
AbhinavKumarTiwari53 <abhinav.tiwari2_cs23@gla.ac.in>
2026-01-19 15:08:58 +0530

    Added our first API

--- src/main/java/org/example/day2part2/controller/home.java
+++ src/main/java/org/example/day2part2/controller/home.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class home {
+
     @GetMapping("/about")
     public StudentModel getStudent(){
         StudentModel student = new StudentModel(1, "Abhinav","example@mail.com");
         return student;
     }
+
 }
